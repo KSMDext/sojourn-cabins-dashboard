154f64b64eb6f3bdbf0a193f85fcce7fa6a40195
hydi-official <esseldacosta00@gmail.com>
2023-08-31 15:03:58 +0000

    dashboard layout updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,45 +1,58 @@
-import React , {useState}from 'react'
-import { FiArrowUp,  FiArrowDown} from 'react-icons/fi';
+import React, { useState } from 'react';
+import { FiArrowUp, FiArrowDown } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import BarGraph from '../components/Charts/BarGraph';
-
-
-import CustomCalendar from '../components/Calendar'
+import CustomCalendar from '../components/Calendar';
 import 'react-calendar/dist/Calendar.css';
 
 const Dashboard = () => {
   const [date] = useState(new Date());
 
- 
-
   return (
-    <div>
-        <div className=" text-2xl ml-6 mt-6">Dashboard</div>
-        <div className=" w-auto mr-6 flex ml-6 justify-between">
-          <Link>
-            <div className=" w-[350px] h-32 bg-white rounded-lg pt-3 pl-4 mx-2"> 
-              <div className="text-xl ">Bookings</div>
-              <div className="text-3xl pt-2">3,120</div>
-              <div className=" flex pt-3 text-sm"><span className="flex pr-2 text-cyan-600">  <FiArrowUp className=" h-5"/>15% </span> last month</div>
-            </div>
-          </Link>
-          <Link>
-            <div className=" w-[350px] h-32 bg-white rounded-lg pt-3 pl-4 mx-2"> 
-              <div className="text-xl ">Check In</div>
-              <div className="text-3xl pt-2">1,748</div>
-              <div className=" flex pt-3 text-sm"><span className="flex pr-2 text-red-700"><FiArrowDown className=" h-5"/> 10% </span> last month</div>
-            </div>
-          </Link>
-          <Link>
-            <div className=" w-[350px] h-32 bg-white rounded-lg pt-3 pl-4 mx-2"> 
-              <div className="text-xl ">Check Out</div>
-              <div className="text-3xl pt-2">989</div>
-              <div className=" flex pt-3 text-sm"><span className="flex pr-2 text-cyan-600"> <FiArrowUp className=" h-5"/>20% </span> last month</div>
-            </div>
-          </Link>
-        </div>
+    <div className="p-4">
+      <div className="text-2xl mt-6">Dashboard</div>
 
-        <div className="w-auto mr-6 bg-white h-96 mt-10 ml-6 rounded-lg flex flex-col justify-center items-center">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 mt-6">
+        <Link to="#">
+          <div className="w-full h-32 bg-white rounded-lg p-4">
+            <div className="text-xl">Bookings</div>
+            <div className="text-3xl pt-2">3,120</div>
+            <div className="flex pt-3 text-sm">
+              <span className="flex pr-2 text-cyan-600">
+                <FiArrowUp className="h-5" /> 15%
+              </span>{' '}
+              last month
+            </div>
+          </div>
+        </Link>
+
+        <Link to="#">
+          <div className="w-full h-32 bg-white rounded-lg p-4">
+            <div className="text-xl">Check In</div>
+            <div className="text-3xl pt-2">1,748</div>
+            <div className="flex pt-3 text-sm">
+              <span className="flex pr-2 text-red-700">
+                <FiArrowDown className="h-5" /> 10%
+              </span>{' '}
+              last month
+            </div>
+          </div>
+        </Link>
+
+        <Link to="#">
+          <div className="w-full h-32 bg-white rounded-lg p-4">
+            <div className="text-xl">Check Out</div>
+            <div className="text-3xl pt-2">989</div>
+            <div className="flex pt-3 text-sm">
+              <span className="flex pr-2 text-cyan-600">
+                <FiArrowUp className="h-5" /> 20%
+              </span>{' '}
+              last month
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div className="w-auto mr-6 bg-white h-96 mt-10 ml-6 rounded-lg flex flex-col justify-center items-center">
   <div className="text-xl mt-0 text-center">
     <h2>CABIN REVENUES/ 24HRS ($)</h2>
   </div>
@@ -70,4 +83,4 @@ const Dashboard = () => {
   )
 }
 
-export default Dashboard  
+export default Dashboard;
